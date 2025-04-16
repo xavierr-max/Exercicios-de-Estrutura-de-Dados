e465c925933ae69c52e146fa73dec67c2c3c2dec
Maxwell Xavier <maxwelldearaujo1017@gmail.com>
2025-04-16 14:30:52 -0300

    Adicao de LinearSearch e SelectionSort

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Search
+    {
+        static int LinearSearch(int[] a, int n, int key)
+        {
+            int index = 0;
+            while(index < n)
+            {
+                if(a[index] == key)
+                    return index;
+                index++;
+            }
+            return -1;
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Search s = new Search();
+            int[] a = {5, 32, 56, 76, 97};
+            int found = LinearSearch(a, 5, 76);
+            System.Console.WriteLine(found);
+
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Sort
+    {
+        public void selectionsort(int[] A, int n)
+        {
+            for(int i = 0; i < n - 1; i++)
+            {
+                
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
