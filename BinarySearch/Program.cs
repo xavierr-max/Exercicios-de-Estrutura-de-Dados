80d9ad4fb7031970789297d6e6055cead321eec7
Maxwell Xavier <maxwelldearaujo1017@gmail.com>
2025-04-17 09:52:05 -0300

    Adicao de BinarySearch

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -2,31 +2,36 @@
 
 namespace MyApp
 {
-    internal class Search
+    public class Search
     {
-        static void binarySearch(int[] A, int n , int key)
+        public int BinarySearch(int[] a, int n, int key)
         {
-            int l = 0;
-            int r = n - 1;
-            while(l <= r)
+            int left = 0;
+            int right = a.Length - 1;
+
+            while (left <= right)
             {
-                int mid = (r + l)/2;
-                if(mid[A] == key)
+                int mid = (left + right) / 2;
+
+                if (a[mid] == key)
                     return mid;
-                if(mid[A] < key)
-                    r = mid + 1
-                if(mid[A] > key)
-                    l = mid - 1
+                if (a[mid] > key)
+                    left = mid + 1;
+                if (a[mid] < key)
+                    right = mid - 1;
             }
-            return -1;
+
+            return -1; // Não encontrado
         }
-        
+
         static void Main(string[] args)
         {
-           Search s = new Search();
-           int[] a = {1, 23, 41, 50, 66, 78, 95}
-           int found = s.binarySearch(a, 7, 50)
-           System.Console.WriteLine(found);
+            Search s = new Search();
+            int[] a = { 1, 23, 41, 50, 66, 78, 95 };
+            int found = s.BinarySearch(a, 7, 50);  // apenas passa o array e o número a buscar
+            System.Console.WriteLine(found);
+
         }
     }
+
 }
